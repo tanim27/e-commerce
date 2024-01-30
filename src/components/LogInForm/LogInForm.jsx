af9e5cf6561b5b1d4687d80ad92a90eabeab075c
Tanim <abdullahtanim27@gmail.com>
2024-01-30 23:34:32 +0600

    update

--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -10,7 +10,7 @@ function LogInForm({ visible, close }) {
         onRequestClose={close}
         style={{
           content: {
-            height: "480px",
+            height: "475px",
             width: "400px",
             top: "50%",
             left: "50%",
